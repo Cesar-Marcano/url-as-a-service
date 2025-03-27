aa510c04bff34cb454ab07ea40c947f6d419e391
Cesar-Marcano <marcanocesar86@gmail.com>
2025-03-27 18:57:15 -0400

    feat: refactor SubscriptionRuleEntity to use enum for rules and map values

--- src/domain/entities/subscription-rules.entity.ts
+++ src/domain/entities/subscription-rules.entity.ts
@@ -1,10 +1,24 @@
-export type SubscriptionRuleValue = string | number | boolean | Date;
+import { SubscriptionEntity } from './subscription.entity'
 
-export class SubscriptionRuleEntity {
+
+export enum SubscriptionRule {
+  MAX_URLS = 'max_urls',
+  MAX_REQUESTS = 'max_requests',
+  MAX_REQUESTS_PER_HOUR = 'max_requests_per_hour',
+}
+
+type SubscriptionRuleValuesMap = {
+  [SubscriptionRule.MAX_URLS]: number;
+  [SubscriptionRule.MAX_REQUESTS]: number;
+  [SubscriptionRule.MAX_REQUESTS_PER_HOUR]: number;
+};
+
+export class SubscriptionRuleEntity<T extends SubscriptionRule> {
   constructor(
     public readonly id: number,
-    public readonly rule: string,
-    public readonly value: SubscriptionRuleValue,
+    public readonly subscription: SubscriptionEntity,
+    public readonly rule: SubscriptionRule,
+    public readonly value: SubscriptionRuleValuesMap[T],
     public readonly createdAt: Date,
     public readonly updatedAt: Date,
   ) {}
