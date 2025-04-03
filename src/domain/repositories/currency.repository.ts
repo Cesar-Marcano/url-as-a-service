0924b8e1e00da34149ebdab3d5f5c46868b45722
Cesar-Marcano <marcanocesar86@gmail.com>
2025-04-03 14:14:03 -0400

    feat(currency-repository): add ICurrencyRepository interface for currency management

---
+++ src/domain/repositories/currency.repository.ts
@@ -0,0 +1,16 @@
+import { CurrencyEntity } from '../entities/currency.entity'
+
+export interface ICurrencyRepository {
+  getCurrencyById(id: number): Promise<CurrencyEntity | null>
+
+  getCurrencyByCode(code: string): Promise<CurrencyEntity | null>
+
+  createCurrency(currency: CurrencyEntity): Promise<CurrencyEntity>
+
+  updateCurrency(
+    id: number,
+    currency: Partial<CurrencyEntity>,
+  ): Promise<CurrencyEntity | null>
+
+  deleteCurrency(id: number): Promise<boolean>
+}
