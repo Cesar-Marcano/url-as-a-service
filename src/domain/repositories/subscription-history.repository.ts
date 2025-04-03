25c5a2bcdebe5df1c2b2fe64cfdc0b7cadb9c0d9
Cesar-Marcano <marcanocesar86@gmail.com>
2025-04-03 14:12:08 -0400

    feat(subscription-history-repository): add interface for managing subscription history records

---
+++ src/domain/repositories/subscription-history.repository.ts
@@ -0,0 +1,28 @@
+import { SubscriptionHistoryEntity } from '../entities/subscription-history.entity'
+
+export interface ISubscriptionHistoryRepository {
+  getSubscriptionRecordById(
+    id: number,
+  ): Promise<SubscriptionHistoryEntity | null>
+
+  getSubscriptionRecordsByUserId(
+    userId: number,
+  ): Promise<SubscriptionHistoryEntity[]>
+
+  getSubscriptionRecordsByUserIdInRange(
+    userId: number,
+    startDate: Date,
+    endDate: Date,
+  ): Promise<SubscriptionHistoryEntity[]>
+
+  createSubscriptionRecord(
+    subscriptionHistory: SubscriptionHistoryEntity,
+  ): Promise<SubscriptionHistoryEntity>
+
+  updateSubscriptionRecord(
+    id: number,
+    subscriptionHistory: Partial<SubscriptionHistoryEntity>,
+  ): Promise<SubscriptionHistoryEntity | null>
+
+  deleteSubscriptionRecord(id: number): Promise<boolean>
+}
