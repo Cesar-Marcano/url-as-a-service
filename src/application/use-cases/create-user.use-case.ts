415c5055c1d8a6d435e54cd0bf237992c718efe5
Cesar-Marcano <marcanocesar86@gmail.com>
2025-04-03 15:07:12 -0400

    feat(create-user-use-case): implement CreateUser use case for user registration

---
+++ src/application/use-cases/create-user.use-case.ts
@@ -0,0 +1,28 @@
+import { UserEntity, UserType } from '../../domain/entities/user.entity'
+import { IUserRepository } from '../../domain/repositories/user.repository'
+import { Email } from '../../domain/value-objects/email.value-object'
+import { Password } from '../../domain/value-objects/password.value-object'
+import { UserDTO } from '../dtos/user.dto'
+import { UseCase } from '../interfaces/use-case.interface'
+import { UserMapper } from '../mappers/user.mapper'
+
+export interface CreateUserInput {
+  email: string
+  password: string
+}
+
+export class CreateUserUseCase implements UseCase<CreateUserInput, UserDTO> {
+  constructor(private readonly userRepository: IUserRepository) {}
+
+  async execute({ email, password }: CreateUserInput): Promise<UserDTO> {
+    const user = UserEntity.create(
+      new Email(email),
+      await Password.create(password),
+      UserType.USER,
+    )
+
+    const newUser = await this.userRepository.createUser(user)
+
+    return UserMapper.toDTO(newUser)
+  }
+}
--- src/domain/entities/user.entity.ts
+++ src/domain/entities/user.entity.ts
@@ -13,16 +13,40 @@ export enum UserType {
 
 export class UserEntity {
   constructor(
-    public readonly id: number,
+    public readonly id: number | null,
     public readonly email: Email,
     public readonly username: Username | null,
     public readonly password: Password,
     public readonly createdAt: Date,
     public readonly updatedAt: Date,
     public readonly urls: UrlEntity[] | null = null,
-    public readonly subscriptionHistory: SubscriptionHistoryEntity[] | null = null,
+    public readonly subscriptionHistory:
+      | SubscriptionHistoryEntity[]
+      | null = null,
     public readonly userType: UserType,
     public readonly isEmailConfirmed: boolean,
     public readonly is2FAEnabled: boolean,
   ) {}
+
+  static create(
+    email: Email,
+    password: Password,
+    userType: UserType,
+  ): UserEntity {
+    const now = new Date()
+
+    return new UserEntity(
+      null,
+      email,
+      null,
+      password,
+      now,
+      now,
+      null,
+      null,
+      userType,
+      false,
+      false,
+    )
+  }
 }
