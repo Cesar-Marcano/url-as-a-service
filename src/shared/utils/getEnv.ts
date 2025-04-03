2847ec088708fbdbb58c6dad369cbaa2f3c65d56
Cesar-Marcano <marcanocesar86@gmail.com>
2025-04-03 16:13:46 -0400

    feat(env-utils): add getEnv function for safe environment variable retrieval

---
+++ src/shared/utils/getEnv.ts
@@ -0,0 +1,31 @@
+import { EnvContext } from '../context/env.context'
+import { MissingEnvError } from '../errors/missing-env.error'
+
+export function getEnv<T>(key: string, defaultValue?: T): T {
+  const value = process.env[key]
+  if (value === undefined) {
+    if (defaultValue === undefined) {
+      throw new MissingEnvError(key)
+    }
+
+    // TODO: Emit a warning if the default value is used using a logger
+    // logger.warn(`Environment variable ${key} is not set. Using default value: ${defaultValue}`)
+
+    return defaultValue
+  }
+
+  try {
+    const type =
+      typeof defaultValue === 'undefined' ? 'string' : typeof defaultValue
+
+    const envContext = new EnvContext(type)
+
+    return envContext.parse(value) as T
+  } catch (error) {
+    if (error instanceof Error)
+      throw new Error(
+        `Error parsing environment variable '${key}': ${error.message}`,
+      )
+    throw new Error('Unknown error parsing environment variable')
+  }
+}
