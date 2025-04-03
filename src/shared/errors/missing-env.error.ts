b4410ced2f7b7d74b7aa1386162047cf03426d82
Cesar-Marcano <marcanocesar86@gmail.com>
2025-04-03 16:10:15 -0400

    feat(errors): add MissingEnvError class for handling missing environment variable errors

---
+++ src/shared/errors/missing-env.error.ts
@@ -0,0 +1,12 @@
+import { BaseError } from '../utils/base-error'
+
+export class MissingEnvError extends BaseError {
+  constructor(env: string) {
+    super({
+      message: `Missing environment variable: ${env}`,
+      statusCode: 500,
+      name: MissingEnvError.name,
+      genericErrorMessage: 'Internal server error.',
+    })
+  }
+}
