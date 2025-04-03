13b65162a202452fc347c9eae686271bdf0e1f0c
Cesar-Marcano <marcanocesar86@gmail.com>
2025-04-03 16:12:54 -0400

    feat(env-strategy): add JsonEnvStrategy class for JSON environment variable parsing

---
+++ src/shared/strategies/json-env.strategy.ts
@@ -0,0 +1,13 @@
+import { EnvStrategy } from '../interfaces/env-strategy.interface'
+
+export class JsonEnvStrategy implements EnvStrategy {
+  parse(value: string): any {
+    try {
+      return JSON.parse(value)
+    } catch (error) {
+      throw new Error(
+        'Unexpected value type in environment variable; Expected a JSON but it cannot be parsed',
+      )
+    }
+  }
+}
