566a8fc9e62e4ac9d8cec13d803fa08c8e64d342
Cesar-Marcano <marcanocesar86@gmail.com>
2025-04-06 08:58:13 -0400

    feat(users): implement UserRepository with createUser method

---
+++ src/infrastructure/repositories/user.repository.ts
@@ -0,0 +1,44 @@
+import { Pool } from 'pg'
+import { UserEntity } from '../../domain/entities/user.entity'
+import { IUserRepository } from '../../domain/repositories/user.repository'
+import { SqlQuery } from '../../shared/interfaces/sql-query.type'
+
+export class UserRepository implements IUserRepository {
+  constructor(
+    private readonly db: Pool,
+    private readonly createUserQuery: SqlQuery<UserEntity>,
+  ) {}
+
+  getUserById(_id: number): Promise<UserEntity | null> {
+    throw new Error('Method not implemented.')
+  }
+
+  getUserByEmail(_email: string): Promise<UserEntity | null> {
+    throw new Error('Method not implemented.')
+  }
+
+  async createUser(user: UserEntity): Promise<UserEntity> {
+    const newUser = await this.createUserQuery(this.db, [
+      user.email,
+      user.password,
+      user.userType,
+    ])
+
+    if (!newUser.rows[0]) {
+      throw new Error('Error creating user')
+    }
+
+    return newUser.rows[0]
+  }
+
+  updateUser(
+    _id: string,
+    _user: Partial<UserEntity>,
+  ): Promise<UserEntity | null> {
+    throw new Error('Method not implemented.')
+  }
+
+  deleteUser(_id: string): Promise<boolean> {
+    throw new Error('Method not implemented.')
+  }
+}
