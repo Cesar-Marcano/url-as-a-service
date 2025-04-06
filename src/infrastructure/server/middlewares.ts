76c8e6d7b00d191e79cd8cfa3458205758cf3ba3
Cesar-Marcano <marcanocesar86@gmail.com>
2025-04-06 10:05:57 -0400

    feat(middlewares): add helmet and morgan middleware for enhanced security and logging

--- src/infrastructure/server/middlewares.ts
+++ src/infrastructure/server/middlewares.ts
@@ -1,6 +1,10 @@
 import express, { Application } from 'express'
+import morgan from 'morgan'
+import helmet from 'helmet'
 
 export function setupMiddlewares(app: Application) {
+  app.use(helmet())
   app.use(express.json({ limit: '10mb' }))
   app.use(express.urlencoded({ limit: '10mb', extended: true }))
+  app.use(morgan('dev'))
 }
