161aceb66ae59b71b85d0839ce4c7fc857a0cdd7
Cesar-Marcano <marcanocesar86@gmail.com>
2025-04-06 22:23:39 -0400

    feat(database): rename user_type to type in createUser SQL query

--- src/infrastructure/database/queries/users/createUser.sql
+++ src/infrastructure/database/queries/users/createUser.sql
@@ -1,5 +1,5 @@
 INSERT INTO users
-  (email, password, user_type)
+  (email, password, type)
     VALUES
   ($1, $2, $3)
-    RETURNING id, email, username, user_type, created_at, updated_at, is_email_confirmed, is_2fa_enabled;
+    RETURNING id, email, username, type, created_at, updated_at, is_email_confirmed, is_2fa_enabled;
