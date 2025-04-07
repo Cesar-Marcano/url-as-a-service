54c9dce95377f7366aa3c75cbdfc9750dbb5ab64
Cesar-Marcano <marcanocesar86@gmail.com>
2025-04-07 18:34:01 -0400

    feat(errors): add NotFoundErrorException class for handling not found errors

---
+++ src/shared/errors/not-found.error.ts
@@ -0,0 +1,21 @@
+import { BaseError } from '../utils/base-error'
+
+export enum NotFoundErrorExceptionScope {
+  DATABASE = 'DATABASE',
+  API = 'API',
+}
+
+export class NotFoundErrorException extends BaseError {
+  constructor(scope: NotFoundErrorExceptionScope, resource: string) {
+    const message = `Resource ${resource} not found in ${scope}`
+    const metadata = { scope, resource }
+
+    super({
+      name: 'NotFoundErrorException',
+      message,
+      statusCode: 404,
+      metadata,
+      genericErrorMessage: 'Resource not found',
+    })
+  }
+}
