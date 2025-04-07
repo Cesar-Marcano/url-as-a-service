7f9590780996dbfbfba3c832d3be24224ea9733a
Cesar-Marcano <marcanocesar86@gmail.com>
2025-04-06 22:26:34 -0400

    feat(controllers): implement CreateUserController for user creation handling

---
+++ src/infrastructure/controllers/createUser.controller.ts
@@ -0,0 +1,39 @@
+import { NextFunction } from 'express'
+import { UserDTO } from '../../application/dtos/user.dto'
+import { CreateUserUseCase } from '../../application/use-cases/create-user.use-case'
+import { UserType } from '../../domain/entities/user.entity'
+import {
+  AuthenticatedRequest,
+  Controller,
+  TypedResponse,
+} from '../../shared/interfaces/controller.interface'
+
+export interface CreateUserDto {
+  email: string
+  password: string
+}
+
+export class CreateUserController
+  implements Controller<unknown, CreateUserDto, unknown, unknown, UserDTO>
+{
+  constructor(private readonly createUserUseCase: CreateUserUseCase) {}
+
+  async handle(
+    req: AuthenticatedRequest<unknown, CreateUserDto, unknown, unknown>,
+    res: TypedResponse<UserDTO>,
+    next: NextFunction
+  ): Promise<UserDTO | void> {
+    try {
+      const { email, password } = req.body
+      const user = await this.createUserUseCase.execute({
+        email,
+        password,
+        userType: UserType.USER,
+      })
+      res.status(201).json(user)
+      return user
+    } catch (error) {
+        next(error)
+    }
+  }
+}
