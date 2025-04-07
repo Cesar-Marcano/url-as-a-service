776dac1911c2aa723329ecf30940e6335a4afade
Cesar-Marcano <marcanocesar86@gmail.com>
2025-04-06 22:27:22 -0400

    feat(server): integrate ConfigService and add error handling middleware

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,14 @@ import './infrastructure/config/env.config'
 import { runMigrations } from './shared/utils/runMigrations'
 import { db } from './infrastructure/database/database.instance'
 import { Server } from './infrastructure/server/server'
+import { ConfigService } from './infrastructure/config/main.config'
 
 async function bootstrap() {
   await runMigrations(db)
 
-  new Server().start(3000)
+  const configService = new ConfigService()
+
+  new Server(configService).start(3000)
 }
 
 void bootstrap()
--- src/infrastructure/server/server.ts
+++ src/infrastructure/server/server.ts
@@ -2,16 +2,18 @@ import express from 'express'
 import { setupMiddlewares } from './middlewares'
 import { setupRouter } from './router'
 import { logger } from '../../shared/utils/logger'
+import { errorHandler } from './middlewares/error.filter'
+import { ConfigService } from '../config/main.config'
 
 export class Server {
   private readonly app = express()
 
-  constructor() {
+  constructor(private readonly configService: ConfigService) {
     setupMiddlewares(this.app)
 
-    // TODO: Setup filters
-    
     setupRouter(this.app)
+
+    this.app.use(errorHandler(this.configService))
   }
 
   public start(port: number): void {
