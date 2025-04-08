fd15b57aa0dab14f7e1a9493a3e493a41b897615
Cesar-Marcano <marcanocesar86@gmail.com>
2025-04-08 15:14:08 -0400

    feat(jwt): add passport-jwt package and implement access token strategy

---
+++ src/infrastructure/strategies/jwt.strategy.ts
@@ -0,0 +1,15 @@
+import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt'
+import { TokenType } from '../../domain/enums/token-type.enum'
+import { JwtPayload } from '../../domain/interfaces/jwt-payload.interface'
+
+export const createAccessTokenStrategy = (secret: string) =>
+  new JwtStrategy(
+    {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: secret,
+    },
+    (payload: JwtPayload, done: Function) => {
+      if (payload.type !== TokenType.ACCESS) return done(null, false)
+      return done(null, payload)
+    },
+  )
