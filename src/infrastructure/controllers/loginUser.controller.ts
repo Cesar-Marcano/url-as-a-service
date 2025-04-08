528839ff0894ce496428c507d8b087b4b9730baa
Cesar-Marcano <marcanocesar86@gmail.com>
2025-04-08 15:55:52 -0400

    feat(login): integrate refresh token generation into login user flow

--- src/infrastructure/controllers/loginUser.controller.ts
+++ src/infrastructure/controllers/loginUser.controller.ts
@@ -1,31 +1,54 @@
 import { NextFunction } from 'express'
 import { UserDTO } from '../../application/dtos/user.dto'
 import { LoginUserUseCase } from '../../application/use-cases/login-user/login-user.use-case'
-import { HydratedRequest, Controller, HydratedResponse } from '../../shared/interfaces/controller.interface'
+import {
+  HydratedRequest,
+  Controller,
+  HydratedResponse,
+} from '../../shared/interfaces/controller.interface'
+import { RefreshTokenUseCase } from '../../application/use-cases/refresh-token/refresh-token.use-case'
+import { UserType } from '../../domain/entities/user.entity'
 
 export interface LoginUserDto {
   email: string
   password: string
 }
 
+export interface LoginResponse {
+  refreshToken: string
+}
+
 export class LoginUserController
-  implements Controller<unknown, LoginUserDto, unknown, unknown, UserDTO>
+  implements Controller<unknown, LoginUserDto, unknown, unknown, LoginResponse>
 {
-  constructor(private readonly loginUserUseCase: LoginUserUseCase) {}
+  constructor(
+    private readonly loginUserUseCase: LoginUserUseCase,
+    private readonly refreshTokenUseCase: RefreshTokenUseCase,
+  ) {}
 
   async handle(
     req: HydratedRequest<unknown, LoginUserDto, unknown, unknown>,
-    res: HydratedResponse<UserDTO>,
+    res: HydratedResponse<LoginResponse>,
     next: NextFunction,
-  ): Promise<UserDTO | void> {
+  ): Promise<LoginResponse | void> {
     try {
       const { email, password } = req.body
       const user = await this.loginUserUseCase.execute({
         email,
         password,
       })
-      res.status(200).json(user)
-      return user
+
+      const refreshToken = await this.refreshTokenUseCase.execute({
+        userId: user.id,
+        email: user.email,
+        role: user.userType as UserType,
+      })
+
+      res.status(200).json({
+        refreshToken,
+      })
+
+      return { refreshToken }
     } catch (error) {
       next(error)
     }
