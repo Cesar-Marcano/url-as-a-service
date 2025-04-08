482baa8ce9350f611dee849d94d274b424a328fd
Cesar-Marcano <marcanocesar86@gmail.com>
2025-04-08 15:10:56 -0400

    feat(token): implement JwtService for signing and verifying JWT tokens

---
+++ src/infrastructure/services/token.service.ts
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken'
+import { JwtPayload } from '../../domain/interfaces/jwt-payload.interface'
+import { TokenType } from '../../domain/enums/token-type.enum'
+import ms from 'ms'
+
+export class JwtService {
+  constructor(
+    private readonly accessSecret: string,
+    private readonly refreshSecret: string,
+  ) {}
+
+  sign(payload: JwtPayload, expiresIn: number | ms.StringValue): string {
+    const secret =
+      payload.type === TokenType.ACCESS ? this.accessSecret : this.refreshSecret
+
+    return jwt.sign(payload, secret, { expiresIn })
+  }
+
+  verify(token: string, type: TokenType): JwtPayload {
+    const secret =
+      type === TokenType.ACCESS ? this.accessSecret : this.refreshSecret
+
+    return jwt.verify(token, secret) as unknown as JwtPayload
+  }
+}
