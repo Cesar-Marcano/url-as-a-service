2e6c42626623ab0087c344a212ac47856d83c35f
Cesar-Marcano <marcanocesar86@gmail.com>
2025-04-08 15:36:51 -0400

    feat(middlewares): add passport initialization middleware

--- src/infrastructure/server/middlewares.ts
+++ src/infrastructure/server/middlewares.ts
@@ -1,10 +1,12 @@
 import express, { Application } from 'express'
 import morgan from 'morgan'
 import helmet from 'helmet'
+import { passport } from '../config/passport.config'
 
 export function setupMiddlewares(app: Application) {
   app.use(helmet())
   app.use(express.json({ limit: '10mb' }))
   app.use(express.urlencoded({ limit: '10mb', extended: true }))
   app.use(morgan('dev'))
+  app.use(passport.initialize())
 }
