565dd0af2c29d542e291b3f21742e31809efbafc
Cesar-Marcano <marcanocesar86@gmail.com>
2025-04-08 16:15:33 -0400

    feat(access-token): implement AccessTokenController

---
+++ src/infrastructure/controllers/accessToken.controller.ts
@@ -0,0 +1,43 @@
+import { NextFunction } from 'express'
+import {
+  HydratedRequest,
+  Controller,
+  HydratedResponse,
+} from '../../shared/interfaces/controller.interface'
+import { AccessTokenUseCase } from '../../application/use-cases/access-token/access-token.use-case'
+
+export interface AccessTokenDto {
+  refreshToken: string
+}
+
+export interface AccessTokenResponse {
+  accessToken: string
+}
+
+export class AccessTokenController
+  implements
+    Controller<unknown, AccessTokenDto, unknown, unknown, AccessTokenResponse>
+{
+  constructor(private readonly accessTokenUseCase: AccessTokenUseCase) {}
+
+  async handle(
+    req: HydratedRequest<unknown, AccessTokenDto, unknown, unknown>,
+    res: HydratedResponse<AccessTokenResponse>,
+    next: NextFunction,
+  ): Promise<AccessTokenResponse | void> {
+    try {
+      const { refreshToken } = req.body
+      const accessToken = await this.accessTokenUseCase.execute({
+        refreshToken,
+      })
+
+      res.status(200).json({
+        accessToken,
+      })
+
+      return { accessToken }
+    } catch (error) {
+      next(error)
+    }
+  }
+}
