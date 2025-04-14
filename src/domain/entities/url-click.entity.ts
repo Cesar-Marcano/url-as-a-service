ed357d95e70437055f07610df20826b148f38589
Cesar-Marcano <marcanocesar86@gmail.com>
2025-04-14 18:05:22 -0400

    feat(url): add urlId property to UrlClickEntity and enhance create method

--- src/domain/entities/url-click.entity.ts
+++ src/domain/entities/url-click.entity.ts
@@ -1,8 +1,26 @@
+import { Relation } from '@domain/value-objects/relation.value-object'
+import { UrlEntity } from './url.entity'
+
 export class UrlClickEntity {
   constructor(
     public readonly id: number,
+    public readonly urlId: Relation<UrlEntity> | null,
     public readonly clickTime: Date,
     public readonly fromIpAddress: string,
     public readonly fromUserAgent: string,
   ) {}
+
+  static create(
+    urlId: number,
+    fromIpAddress: string,
+    fromUserAgent: string,
+  ): UrlClickEntity {
+    return new UrlClickEntity(
+      0,
+      new Relation(urlId),
+      new Date(),
+      fromIpAddress,
+      fromUserAgent,
+    )
+  }
 }
