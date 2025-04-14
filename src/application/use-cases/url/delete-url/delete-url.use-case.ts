302954757b609d1c702c8df165250eaa366dbbaf
Cesar-Marcano <marcanocesar86@gmail.com>
2025-04-14 08:53:51 -0400

    feat(url): implement DeleteUrlUseCase for deleting URLs with authorization checks

---
+++ src/application/use-cases/url/delete-url/delete-url.use-case.ts
@@ -0,0 +1,37 @@
+import { UseCase } from '@app/interfaces/use-case.interface'
+import { DeleteUrlInput } from './delete-url.input'
+import { IUrlRepository } from '@domain/repositories/url.repository'
+import { UserType } from '@domain/entities/user.entity'
+import { UnauthorizedErrorException } from '@shared/errors/unauthorized.error'
+import {
+  NotFoundErrorException,
+  NotFoundErrorExceptionScope,
+} from '@shared/errors/not-found.error'
+
+export class DeleteUrlUseCase implements UseCase<DeleteUrlInput, boolean> {
+  constructor(private readonly urlRepository: IUrlRepository) {}
+
+  async execute(input: DeleteUrlInput): Promise<boolean> {
+    const url = await this.urlRepository.findById(input.urlId)
+
+    if (!url)
+      throw new NotFoundErrorException(
+        NotFoundErrorExceptionScope.DATABASE,
+        'url',
+      )
+
+    const canBypassAuthorCheck =
+      input.user.userType === UserType.ADMIN ||
+      input.user.userType === UserType.MODERATOR
+
+    if (url.author !== input.user.id || !canBypassAuthorCheck) {
+      throw new UnauthorizedErrorException(
+        "You don't have enough permissions to delete an url that you don't own.",
+      )
+    }
+
+    await this.urlRepository.delete(input.urlId)
+
+    return true
+  }
+}
