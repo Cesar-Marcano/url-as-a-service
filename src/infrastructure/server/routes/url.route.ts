992f38816d87ec781c694a946b033ff0ae201ec5
Cesar-Marcano <marcanocesar86@gmail.com>
2025-04-14 12:10:39 -0400

    feat(url): add URL route for creating new URLs with authentication

---
+++ src/infrastructure/server/routes/url.route.ts
@@ -0,0 +1,33 @@
+import { Router } from 'express'
+import { db } from '@infra/database/database.instance'
+import { jwtAuthMiddleware } from '@shared/middlewares/auth.middleware'
+import { registerRoutes } from '@shared/utils/register-routes'
+import { CreateUrlController } from '@infra/controllers/urls/createUrl/createUrl.controller'
+import { CreateUrlUseCase } from '@app/use-cases/url/create-url/create-url.use-case'
+import { UrlRepository } from '@infra/repositories/url.repository'
+
+// Route settings
+export const router = Router()
+export const name = 'url' // route prefix
+
+// Respositories
+const urlRepository = new UrlRepository(db)
+
+// External services
+// const cacheService = new CacheService(cache)
+
+// Use cases
+const createUrlUseCase = new CreateUrlUseCase(urlRepository)
+
+// Controllers
+const createUrlController = new CreateUrlController(createUrlUseCase)
+
+// Route definitions
+registerRoutes(router, [
+  {
+    path: '/create',
+    controller: createUrlController,
+    method: 'post',
+    middlewares: [jwtAuthMiddleware],
+  },
+])
