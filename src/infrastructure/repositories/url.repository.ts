fe57a9c47cecb0e1193d3accc84381216e85f0cd
Cesar-Marcano <marcanocesar86@gmail.com>
2025-04-14 11:45:08 -0400

    feat(url): implement UrlRepository with methods for finding and creating URLs

---
+++ src/infrastructure/repositories/url.repository.ts
@@ -0,0 +1,77 @@
+import { UrlMapper } from '@app/mappers/url.mapper'
+import { UrlEntity } from '@domain/entities/url.entity'
+import { IUrlRepository } from '@domain/repositories/url.repository'
+import { SqlQuery } from '@shared/interfaces/sql-query.type'
+import { createSqlRunner, SqlRunnerScope } from '@shared/utils/createSqlRunner'
+import { Pool } from 'pg'
+
+export class UrlRepository implements IUrlRepository {
+  // DQL
+  private readonly findUrlBySlugQuery: SqlQuery
+
+  // DML
+  private readonly createUrlQuery: SqlQuery
+
+  constructor(private readonly db: Pool) {
+    // DQL
+    this.findUrlBySlugQuery = createSqlRunner(
+      'urls/findUrlBySlug.sql',
+      SqlRunnerScope.Queries,
+    )
+
+    // DML
+    this.createUrlQuery = createSqlRunner(
+      'urls/createUrl.sql',
+      SqlRunnerScope.Queries,
+    )
+  }
+
+  findById(_id: number): Promise<UrlEntity | null> {
+    throw new Error('Method not implemented.')
+  }
+
+  async findBySlug(slug: string): Promise<UrlEntity | null> {
+    const result = await this.findUrlBySlugQuery(this.db, [slug])
+
+    return UrlMapper.fromDB(result.rows[0])
+  }
+
+  findByLongUrl(_originalUrl: string): Promise<UrlEntity | null> {
+    throw new Error('Method not implemented.')
+  }
+
+  async create(url: UrlEntity): Promise<UrlEntity> {
+    const result = await this.createUrlQuery(this.db, [
+      url.slug,
+      url.originalUrl,
+      url.author!.id,
+      url.expirationDate,
+    ])
+
+    return UrlMapper.fromDB(result.rows[0])
+  }
+
+  update(_url: Partial<UrlEntity>): Promise<UrlEntity> {
+    throw new Error('Method not implemented.')
+  }
+
+  delete(_id: number): Promise<void> {
+    throw new Error('Method not implemented.')
+  }
+
+  getAll(): Promise<UrlEntity[]> {
+    throw new Error('Method not implemented.')
+  }
+
+  count(): Promise<number> {
+    throw new Error('Method not implemented.')
+  }
+
+  getAllByUserId(_userId: number): Promise<UrlEntity[]> {
+    throw new Error('Method not implemented.')
+  }
+
+  countByUserId(_userId: number): Promise<number> {
+    throw new Error('Method not implemented.')
+  }
+}
