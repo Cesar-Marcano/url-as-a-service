8049790493572b54ddec4a27e56877344f1f109a
Cesar-Marcano <marcanocesar86@gmail.com>
2025-04-15 19:00:13 -0400

    feat(analytics): implement GeoAnalyticsStrategy for geo-based analytics retrieval

---
+++ src/application/use-cases/url-history/get-analytics/strategies/geo-analytics.strategy.ts
@@ -0,0 +1,22 @@
+import { IUrlClickRepository } from '@domain/repositories/url-click.repository'
+import { GetAnalyticsInput, GetGeoAnalyticsInput } from '../get-analytics.input'
+import {
+  GetAnalyticsOutput,
+  GetAnalyticsStrategy,
+} from '../get-analytics.strategy'
+
+export class GeoAnalyticsStrategy implements GetAnalyticsStrategy {
+  constructor(private urlClickRepository: IUrlClickRepository) {}
+
+  canHandle(input: GetAnalyticsInput): boolean {
+    return 'urlId' in input
+  }
+
+  async execute(input: GetAnalyticsInput): Promise<GetAnalyticsOutput> {
+    const { urlId } = input as GetGeoAnalyticsInput
+
+    const data = await this.urlClickRepository.getGeoAnalytics(urlId)
+
+    return data
+  }
+}
