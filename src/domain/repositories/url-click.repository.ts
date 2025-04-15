f2041ca34e9309eb3920fb70e0b3313ce6a52b9f
Cesar-Marcano <marcanocesar86@gmail.com>
2025-04-15 18:41:34 -0400

    refactor(repository): simplify return types in IUrlClickRepository interface

--- src/domain/repositories/url-click.repository.ts
+++ src/domain/repositories/url-click.repository.ts
@@ -1,22 +1,31 @@
 import { UrlClickEntity } from '@domain/entities/url-click.entity'
 
+export type TotalClickCountByUrls = Array<{
+  url_id: number
+  click_count: number
+}>
+
+export type ClickCountPerTime = Array<{ time: string; count: number }>
+
+export type GeoAnalytics = Array<{
+  country: string
+  city: string
+  count: number
+}>
+
+export type UserAgentAnalytics = Array<{ userAgent: string; count: number }>
+
 export interface IUrlClickRepository {
   registerClick(urlClick: UrlClickEntity): Promise<void>
 
-  getTotalClickCountByUrls(
-    userId: number,
-  ): Promise<Array<{ url_id: number; click_count: number }>>
+  getTotalClickCountByUrls(userId: number): Promise<TotalClickCountByUrls>
 
   getClickCountPerTime(
     timeUnit: 'day' | 'week' | 'month',
     urlId: number,
-  ): Promise<Array<{ time: string; count: number }>>
+  ): Promise<ClickCountPerTime>
 
-  getGeoAnalytics(
-    urlId: number,
-  ): Promise<Array<{ country: string; city: string; count: number }>>
+  getGeoAnalytics(urlId: number): Promise<GeoAnalytics>
 
-  getUserAgentAnalytics(
-    urlId: number,
-  ): Promise<Array<{ userAgent: string; count: number }>>
+  getUserAgentAnalytics(urlId: number): Promise<UserAgentAnalytics>
 }
